f4116bdcfe118424d8a9fd7e23a65be276103589
Geraldine <c4dn5wld3@vossie.net>
2026-01-20 19:06:17 +0200

    Adding appointments to array

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -11,4 +11,21 @@ export class AppointmentListComponent {
   newAppointmentDate: Date = new Date();
 
   appointments: Appointment[] = [];
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+
+      this.appointments.push(newAppointment);
+
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+
+      alert(this.appointments.length + ' appointment(s) scheduled.');
+    }
+  }
 }
